642bf2035ae1a8f7058966e0728995323f15bc7a
eljamm <fedi.jamoussi@protonmail.ch>
2026-04-08 17:11:48 +0200

    fixup! feat: init consumer flake template

--- templates/consumer/recipes/apps/hello-app/recipe.nix
+++ templates/consumer/recipes/apps/hello-app/recipe.nix
@@ -6,13 +6,18 @@
 }:
 
 {
-  container = {
-    imageConfig.CMD = lib.mkForce [
-      "hola"
-    ];
-  };
+  services = {
+    runtimes = {
+      container = {
+        enable = true;
+        imageConfig.CMD = lib.mkForce [
+          "hola"
+        ];
+      };
 
-  nixos = {
-    enable = lib.mkForce false;
+      nixos = {
+        enable = lib.mkForce false;
+      };
+    };
   };
 }
