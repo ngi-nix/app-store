phanirithvij/nix-forge

+dev/adaptability(ui)(NgiGrants): no need to hardcode grant names in the UI

ui/src/Main/Config/App.elm
@@ -1,5 +1,6 @@
 module Main.Config.App exposing (..)
 
+import Dict exposing (Dict)
 import Json.Decode as Decode exposing (Decoder)
 import Main.Helpers.String exposing (..)
 
@@ -11,12 +12,7 @@ type alias App =
     , app_programs : AppPrograms
     , app_container : AppContainer
     , app_vm : AppNixosVm
-    , app_ngi : AppNgi
-    }
-
-
-type alias AppNgi =
-    { grants : AppNgiSubgrants
+    , app_ngi : Ngi
     }
 
 
@@ -40,14 +36,6 @@ type alias AppNixosVm =
     }
 
 
-type alias AppNgiSubgrants =
-    { commons : List String
-    , core : List String
-    , entrust : List String
-    , review : List String
-    }
-
-
 type alias AppName =
     String
 
@@ -61,7 +49,7 @@ decodeApp =
         (Decode.field "programs" decodeAppPrograms)
         (Decode.field "container" decodeAppContainer)
         (Decode.field "nixos" decodeAppNixosVm)
-        (Decode.field "ngi" decodeAppNgi)
+        (Decode.field "ngi" decodeNgi)
 
 
 decodeAppName : Decoder AppName
@@ -95,19 +83,28 @@ decodeAppNixosVm =
         (Decode.field "enable" Decode.bool)
 
 
-decodeAppNgi : Decoder AppNgi
-decodeAppNgi =
-    Decode.map AppNgi
-        (Decode.field "grants" decodeAppNgiSubgrants)
+type alias Ngi =
+    { grants : NgiGrants
+    }
 
 
-decodeAppNgiSubgrants : Decoder AppNgiSubgrants
-decodeAppNgiSubgrants =
-    Decode.map4 AppNgiSubgrants
-        (Decode.field "Commons" (Decode.list Decode.string))
-        (Decode.field "Core" (Decode.list Decode.string))
-        (Decode.field "Entrust" (Decode.list Decode.string))
-        (Decode.field "Review" (Decode.list Decode.string))
+decodeNgi : Decoder Ngi
+decodeNgi =
+    Decode.map Ngi
+        (Decode.field "grants" decodeNgiGrants)
+
+
+type alias NgiGrants =
+    Dict String NgiGrantsSub
+
+
+decodeNgiGrants : Decoder NgiGrants
+decodeNgiGrants =
+    Decode.dict (Decode.list Decode.string)
+
+
+type alias NgiGrantsSub =
+    List String
 
 
 type AppOutput

ui/src/Main/View.elm
@@ -434,7 +434,10 @@ viewAppNgiGrants model pageApp =
         subgrants =
             pageApp.pageApp_app.app_ngi.grants
     in
-    if hasAnyGrants subgrants then
+    if subgrants |> Dict.values |> List.concat |> List.isEmpty then
+        text ""
+
+    else
         div
             [ class "box-container target-highlight mb-3"
             , id "grants"
@@ -457,46 +460,34 @@ viewAppNgiGrants model pageApp =
                     []
                 ]
             , div []
-                [ viewGrantCategory "Commons" subgrants.commons
-                , viewGrantCategory "Core" subgrants.core
-                , viewGrantCategory "Entrust" subgrants.entrust
-                , viewGrantCategory "Review" subgrants.review
-                ]
+                (subgrants
+                    |> Dict.toList
+                    |> List.map viewGrantCategory
+                )
             ]
 
-    else
-        text ""
 
-
-hasAnyGrants : AppNgiSubgrants -> Bool
-hasAnyGrants subgrants =
-    not (List.isEmpty subgrants.commons)
-        || not (List.isEmpty subgrants.core)
-        || not (List.isEmpty subgrants.entrust)
-        || not (List.isEmpty subgrants.review)
-
-
-viewGrantCategory : String -> List String -> Html msg
-viewGrantCategory categoryName grants =
-    if List.isEmpty grants then
+viewGrantCategory : ( String, List String ) -> Html msg
+viewGrantCategory ( grant, subgrants ) =
+    if List.isEmpty subgrants then
         text ""
 
     else
         div [ class "container row mb-1" ]
-            [ small [ class "col-6" ] [ text categoryName ]
+            [ small [ class "col-6" ] [ text grant ]
             , ul [ class "col" ]
                 (List.map
-                    (\grantName ->
+                    (\subgrant ->
                         li [ class "list-group-item bg-transparent mb-1" ]
                             [ a
-                                [ href ("https://nlnet.nl/project/" ++ grantName ++ "/")
+                                [ href ("https://nlnet.nl/project/" ++ subgrant ++ "/")
                                 , target "_blank"
                                 , rel "noopener noreferrer"
                                 ]
-                                [ text grantName ]
+                                [ text subgrant ]
                             ]
                     )
-                    grants
+                    subgrants
                 )
             ]
 

ngi-nix/app-store

ui/src/Main/Config/App.elm
@@ -1,5 +1,6 @@
 module Main.Config.App exposing (..)
 
+import Dict exposing (Dict)
 import Json.Decode as Decode exposing (Decoder)
 import Main.Helpers.String exposing (..)
 
@@ -11,13 +12,8 @@ type alias App =
     , app_programs : AppPrograms
     , app_container : AppContainer
     , app_vm : AppNixosVm
-    , app_ngi : AppNgi
+    , app_ngi : Ngi
     , app_links : AppLinks
-    }
-
-
-type alias AppNgi =
-    { grants : AppNgiSubgrants
     }
 
 
@@ -38,21 +34,6 @@ type alias AppContainer =
 
 type alias AppNixosVm =
     { enable : Bool
-    }
-
-
-type alias AppNgiSubgrants =
-    { commons : List String
-    , core : List String
-    , entrust : List String
-    , review : List String
-    }
-
-
-type alias AppLinks =
-    { website : Maybe String
-    , docs : Maybe String
-    , source : Maybe String
     }
 
 
@@ -79,7 +60,7 @@ decodeApp =
         (Decode.field "programs" decodeAppPrograms)
         (Decode.field "container" decodeAppContainer)
         (Decode.field "nixos" decodeAppNixosVm)
-        (Decode.field "ngi" decodeAppNgi)
+        (Decode.field "ngi" decodeNgi)
         (Decode.field "links" decodeAppLinks)
 
 
@@ -114,19 +95,43 @@ decodeAppNixosVm =
         (Decode.field "enable" Decode.bool)
 
 
-decodeAppNgi : Decoder AppNgi
-decodeAppNgi =
-    Decode.map AppNgi
-        (Decode.field "grants" decodeAppNgiSubgrants)
+type alias Ngi =
+    { ngi_grants : NgiGrants
+    }
 
 
-decodeAppNgiSubgrants : Decoder AppNgiSubgrants
-decodeAppNgiSubgrants =
-    Decode.map4 AppNgiSubgrants
-        (Decode.field "Commons" (Decode.list Decode.string))
-        (Decode.field "Core" (Decode.list Decode.string))
-        (Decode.field "Entrust" (Decode.list Decode.string))
-        (Decode.field "Review" (Decode.list Decode.string))
+decodeNgi : Decoder Ngi
+decodeNgi =
+    Decode.map Ngi
+        (Decode.field "grants" decodeNgiGrants)
+
+
+type alias NgiGrants =
+    Dict NgiGrantName NgiSubgrants
+
+
+type alias NgiGrantName =
+    String
+
+
+decodeNgiGrants : Decoder NgiGrants
+decodeNgiGrants =
+    Decode.dict (Decode.list Decode.string)
+
+
+type alias NgiSubgrants =
+    List NgiSubgrantName
+
+
+type alias NgiSubgrantName =
+    String
+
+
+type alias AppLinks =
+    { website : Maybe String
+    , docs : Maybe String
+    , source : Maybe String
+    }
 
 
 decodeAppLinks : Decoder AppLinks

ui/src/Main/View.elm
@@ -445,9 +445,12 @@ viewAppNgiGrants : Model -> PageApp -> Html msg
 viewAppNgiGrants model pageApp =
     let
         subgrants =
-            pageApp.pageApp_app.app_ngi.grants
+            pageApp.pageApp_app.app_ngi.ngi_grants
     in
-    if hasAnyGrants subgrants then
+    if subgrants |> Dict.values |> List.concat |> List.isEmpty then
+        text ""
+
+    else
         div
             [ class "box-container target-highlight mb-3"
             , id "grants"
@@ -470,46 +473,34 @@ viewAppNgiGrants model pageApp =
                     []
                 ]
             , div []
-                [ viewGrantCategory "Commons" subgrants.commons
-                , viewGrantCategory "Core" subgrants.core
-                , viewGrantCategory "Entrust" subgrants.entrust
-                , viewGrantCategory "Review" subgrants.review
-                ]
+                (subgrants
+                    |> Dict.toList
+                    |> List.map viewGrantCategory
+                )
             ]
 
-    else
-        text ""
 
-
-hasAnyGrants : AppNgiSubgrants -> Bool
-hasAnyGrants subgrants =
-    not (List.isEmpty subgrants.commons)
-        || not (List.isEmpty subgrants.core)
-        || not (List.isEmpty subgrants.entrust)
-        || not (List.isEmpty subgrants.review)
-
-
-viewGrantCategory : String -> List String -> Html msg
-viewGrantCategory categoryName grants =
-    if List.isEmpty grants then
+viewGrantCategory : ( NgiGrantName, List NgiSubgrantName ) -> Html msg
+viewGrantCategory ( grant, subgrants ) =
+    if List.isEmpty subgrants then
         text ""
 
     else
         div [ class "container row mb-1" ]
-            [ small [ class "col-6" ] [ text categoryName ]
+            [ small [ class "col-6" ] [ text grant ]
             , ul [ class "col" ]
                 (List.map
-                    (\grantName ->
+                    (\subgrant ->
                         li [ class "list-group-item bg-transparent mb-1" ]
                             [ a
-                                [ href ("https://nlnet.nl/project/" ++ grantName ++ "/")
+                                [ href ("https://nlnet.nl/project/" ++ subgrant ++ "/")
                                 , target "_blank"
                                 , rel "noopener noreferrer"
                                 ]
-                                [ text grantName ]
+                                [ text subgrant ]
                             ]
                     )
-                    grants
+                    subgrants
                 )
             ]
 